19cd80c7863fa18cf80edb599a0e4f91359801b4
mmgithub1978 <45688327+mmgithub1978@users.noreply.github.com>
2018-12-07 15:19:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World from Jenkins and Docker 3");
+    response.end("Hello World from Jenkins and Docker 4");
 
 });
 
